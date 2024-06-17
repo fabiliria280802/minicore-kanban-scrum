7dfde6e2db40e0a3a1a6d0762eb1c10feb46d69d
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-06-16 22:53:45 -0500

    solving instance bug

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
-import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,29 +9,4 @@ import { AuthenticationResult } from '@azure/msal-browser';
 export class AppComponent {
   title = 'frontend';
 
-  constructor(private msalService: MsalService){
-    this.msalService.instance.handleRedirectPromise().then(res=>{
-      if(res !=null && res.account !=null){
-        this.msalService.instance.setActiveAccount(res.account);
-      }
-    })
-  }
-  ngOnInit(): void{
-    this.msalService.instance.handleRedirectPromise().then(res=>{
-      if(res){
-        console.log(res);
-      }
-    })
-
-  }
-  isLoggedIn(): boolean{
-    return this.msalService.instance.getActiveAccount() != null
-  }
-
-  login(){
-    this.msalService.loginRedirect();
-  }
-  logout(){
-    this.msalService.logout();
-  }
 }
